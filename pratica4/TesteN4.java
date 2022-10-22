6d8377040671067d4fa016d651114bcde130fd6d
JohnyLPM <joaoluisparreira@gmail.com>
2022-10-22 19:33:56 +0100

    random previous test

---
+++ pratica4/TesteN4.java
@@ -0,0 +1,9 @@
+public class TesteN4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        OperacoesAritmeticas op = new OperacoesAritmeticas();
+        System.out.println("Soma: " + op.sum(a, b));
+        System.out.println("Divisão: " + op.div(a, b));
+    }
+}
