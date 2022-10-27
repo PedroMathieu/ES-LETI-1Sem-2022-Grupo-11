bc15167c9199cecbda970eb998b14450e12dd296
JohnyLPM <joaoluisparreira@gmail.com>
2022-10-27 14:07:39 +0100

    Testing images in Swing

--- swingTest/swingTest/src/App.java
+++ swingTest/swingTest/src/App.java
@@ -1,54 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Timer;
-import java.util.TimerTask;
+//import java.util.Timer;
+//import java.util.TimerTask;
 
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame();
-        Icon icon = new ImageIcon("kappa.png");
-        JButton button = new JButton(icon);
-        button.setBounds(200, 200, 100, 100);
+        JFrame frame = new JFrame("Test Frame");
 
-        frame.add(button);
-        frame.setSize(500, 500);
+        
+        ImageIcon icon = new ImageIcon("src/kappa.png");
+        JLabel testLabel = new JLabel("", icon, JLabel.CENTER);
+        frame.add(testLabel);
+
+        frame.setSize(600, 600);
         frame.setLayout(null);
         frame.setVisible(true);
-
-        /*
-         * button.addActionListener(new ActionListener() {
-         * public void actionPerformed(ActionEvent e) {
-         * int rand1 = (int)(250*Math.random());
-         * int rand2 = (int)(250*Math.random());
-         * button.setBounds(rand1, rand2, 100, 100);
-         * String label = button.getText();
-         * button.setText(label + "A");
-         * label = button.getText();
-         * System.out.println("Current button label is: " + label);
-         * }
-         * });
-         */
-
-        /*
-         * Timer timer = new Timer();
-         * int begin = 1000;
-         * int timeInt = 10 * 100;
-         * timer.scheduleAtFixedRate(new TimerTask(){
-         * public void run(){
-         * button.setBounds((int)(250*Math.random()), (int)(250*Math.random()), 100,
-         * 100);
-         * }
-         * }, begin, timeInt);
-         */
-
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                button.setBounds((int) (250 * Math.random()), (int) (250 * Math.random()), 100, 100);
-            }
-        });
-    }
-
-    public static void createFrame() {
     }
 }
---
+++ swingTest/swingTest/src/SwingControlDemo.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class SwingControlDemo {
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+ 
+    public SwingControlDemo(){
+       prepareGUI();
+    }
+    public static void main(String[] args){
+       SwingControlDemo  swingControlDemo = new SwingControlDemo();      
+       swingControlDemo.showImageIconDemo();
+    }
+    private void prepareGUI(){
+       mainFrame = new JFrame("Java Swing Examples");
+       mainFrame.setSize(400,400);
+       mainFrame.setLayout(new GridLayout(3, 1));
+       
+       mainFrame.addWindowListener(new WindowAdapter() {
+          public void windowClosing(WindowEvent windowEvent){
+             System.exit(0);
+          }        
+       });    
+       headerLabel = new JLabel("", JLabel.CENTER);        
+       statusLabel = new JLabel("",JLabel.CENTER);    
+       statusLabel.setSize(350,100);
+ 
+       controlPanel = new JPanel();
+       controlPanel.setLayout(new FlowLayout());
+ 
+       mainFrame.add(headerLabel);
+       mainFrame.add(controlPanel);
+       mainFrame.add(statusLabel);
+       mainFrame.setVisible(true);  
+    }
+    // Returns an ImageIcon, or null if the path was invalid. 
+    private static ImageIcon createImageIcon(String path,
+       String description) {
+       java.net.URL imgURL = SwingControlDemo.class.getResource(path);
+       
+       if (imgURL != null) {
+          return new ImageIcon(imgURL, description);
+       } else {            
+          System.err.println("Couldn't find file: " + path);
+          return null;
+       }
+    }
+    private void showImageIconDemo(){
+       headerLabel.setText("Control in action: ImageIcon"); 
+       ImageIcon icon = createImageIcon("src/kappa.png","Java");
+ 
+       JLabel commentlabel = new JLabel("", icon,JLabel.CENTER);
+       controlPanel.add(commentlabel);
+       mainFrame.setVisible(true);  
+    }
+ }
