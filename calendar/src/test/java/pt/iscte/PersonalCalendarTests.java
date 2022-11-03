f76c8cec6dc188faaae3f048e427ee5e8f8f7a68
JohnyLPM <joaoluisparreira@gmail.com>
2022-11-03 00:42:39 +0000

    Create PersonalCalendarTests.java

---
+++ calendar/src/test/java/pt/iscte/PersonalCalendarTests.java
@@ -0,0 +1,11 @@
+package pt.iscte;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PersonalCalendarTests {
+    @Test
+    public void test1(){
+        assert
+    }
+}
